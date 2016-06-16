7fd18572b867ab0083ed67dbd5cb8424fe2dc3f8
Matt <git@skypage.33mail.com>
2016-06-15 21:52:09 -0700

    New API method for feedback form

--- shared/src/main/scala/spa/shared/Api.scala
+++ shared/src/main/scala/spa/shared/Api.scala
@@ -5,4 +5,6 @@ import scala.concurrent.{Future}
 trait Api {
   //Update a list of links
   def updateNewsList(tabId: String, contentType: String): Future[Seq[LinkObject]]
+
+  def submitFeedback(name: String, email: String, subject: String, message: String): Future[Boolean]
 }
