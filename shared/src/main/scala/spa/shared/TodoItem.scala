f5624d5022b37b673ada44a67f7637a6ba22bf42
Matt <git@skypage.33mail.com>
2016-06-28 16:06:48 -0700

    Add shared email validation function

--- shared/src/main/scala/spa/shared/TodoItem.scala
+++ shared/src/main/scala/spa/shared/TodoItem.scala
@@ -92,6 +92,18 @@ object TabFeedSources {
   */
 }
 
+object EmailValidation {
+  //Below regex based on W3C recommendation here: https://www.w3.org/TR/html5/forms.html#valid-e-mail-address
+  private val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
+
+  def isValid(e: String): Boolean = e match{
+    case null                                           => false
+    case e if e.trim.isEmpty                            => false
+    case e if emailRegex.findFirstMatchIn(e).isDefined  => true
+    case _                                              => false
+  }
+}
+
 case class EmailFormData(name: String = "Anonymous",
                          email: String = "anonymous@internet-abridged.com",
                          subject: String, message: String)
