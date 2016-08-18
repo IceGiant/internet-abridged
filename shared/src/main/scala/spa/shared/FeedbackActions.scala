03b10e7b4d520387842405c36b59d2c47682c156
Matt <git@skypage.33mail.com>
2016-08-17 19:30:11 -0700

    Remove extra newlines

--- shared/src/main/scala/spa/shared/FeedbackActions.scala
+++ shared/src/main/scala/spa/shared/FeedbackActions.scala
@@ -3,21 +3,6 @@ package spa.shared
 import boopickle.Default._
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 object EmailValidation {
   //Below regex based on W3C recommendation here: https://www.w3.org/TR/html5/forms.html#valid-e-mail-address
   private val emailRegex = """^[a-zA-Z0-9\.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$""".r
